3d451f0bd0120b2761ae946a918f619de7c29ed3
Zadyvalo2023 <av1469400@gmail.com>
2023-06-28 17:58:11 +0300

    My homeWork

---
+++ Task_34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными
+//  числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Creat class 
+        int[] numbers = new int[10];
+        Random random = new Random();
+        // Complite array randow numbers 
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+        // Подсчитываем количество четных чисел в массиве
+        int evenCount = 0;
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        // Вывдим результат
+    Console.WriteLine("Количество четных чисел в массиве:" + evenCount);
+    }
+}
---
+++ Task_36/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[10]; // Создаем массив Create array
+        Random random = new Random ();// Jenerate random numbers 
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(1, 101);// Генерируем случайоное число с одного до сотни
+        }
+    
+        // Находим сумму элементов на нечетных points 
+        int sum = 0;
+        for (int i = 1; i < arr.Length; i +=2)
+        {
+            sum += arr[i];
+        }
+        Console.WriteLine("Сумма элументов на нечетных позициях: " + sum);
+
+    }
+}
